c1ef4ae3b86ef7f2897d3b8523ade12c7c4eee36
Alin Popa <alin.popa@gmail.com>
2014-07-17 11:56:21 +0300

    Interval to 5 minutes.

--- src/main/scala/org/twm/keepalive/TwmKeepAlive.scala
+++ src/main/scala/org/twm/keepalive/TwmKeepAlive.scala
@@ -13,15 +13,12 @@ object TwmKeepAlive {
 
     val pipeline = sendReceive
 
-    system.scheduler.schedule(initialDelay = 0.seconds, interval = 5.seconds, new Runnable {
+    system.scheduler.schedule(initialDelay = 0.seconds, interval = 5.minutes, new Runnable {
       override def run(): Unit = {
         pipeline(Get("http://twm.herokuapp.com/keep-alive")).onComplete{
           case completed => println(s"Completed: $completed")
         }
       }
     })
-
-    //val response1 = Observable.from(pipeline(Get("https://api.github.com/users")))
-    //val response2 = Observable.from(pipeline(Get("https://api.github.com/users/octocat/orgs")))
   }
 }
